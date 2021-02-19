46851eea9158aa6a7255c936f3e5be682eb9e5d1
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-02-19 11:58:12 -0600

    dwa

---
+++ data.js
@@ -0,0 +1,67 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-earlychildhood",
+      "name": "EarlyChildhood",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.1260823161774614,
+        "pitch": 0.003879540431874773,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 3.115015188083401,
+          "pitch": 0.07845268278919626,
+          "title": "ECE Intro",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.535407001123774,
+          "pitch": 0.0867276194503539,
+          "title": "ECE1",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.6180044910913658,
+          "pitch": 0.11427445953557758,
+          "title": "ECE2",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.276820471915136,
+          "pitch": 0.08853700390226216,
+          "title": "ECE3",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
