83aea2cbfe958c0e428f2c2ff0083874f74e0f0f
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-02-19 12:22:29 -0600

    Update data.js

--- data.js
+++ data.js
@@ -34,25 +34,25 @@ var APP_DATA = {
           "yaw": 3.115015188083401,
           "pitch": 0.07845268278919626,
           "title": "ECE Intro",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1HuVy2v6WqnG_-x11RgbSHVyJAgqRYzOn/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": -2.535407001123774,
           "pitch": 0.0867276194503539,
           "title": "ECE1",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/13rRz57d80jXIHCGLd3DIZ_sB5ndPjlr3/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": -1.6180044910913658,
           "pitch": 0.11427445953557758,
           "title": "ECE2",
-          "text": "Text"
+          "text": '<iframe src="https://drive.google.com/file/d/1bfjs3x9cFhRejcDOtyz6DjfvHal6G1yU/preview" width="640" height="480"></iframe>'
         },
         {
           "yaw": 2.276820471915136,
           "pitch": 0.08853700390226216,
           "title": "ECE3",
-          "text": "Text"
+          "text":'<iframe src="https://drive.google.com/file/d/1qlA0W_SByVxSEQTPEaB0Rrqnmy1zhSt5/preview" width="640" height="480"></iframe>'
         }
       ]
     }
